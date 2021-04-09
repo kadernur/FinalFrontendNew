8a642b7972961aed02ecf8e0d61fc65a9ea0d404
Kader <Lenovo@ACER-COMPUTER>
2021-04-09 04:16:39 +0300

    19.gün sonu

--- northwind/src/app/components/product/product.component.ts
+++ northwind/src/app/components/product/product.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Product } from 'src/app/models/product';
+import { CartService } from 'src/app/services/cart.service';
 
 import { ProductService } from 'src/app/services/product.service';
 
@@ -14,10 +16,13 @@ import { ProductService } from 'src/app/services/product.service';
 export class ProductComponent implements OnInit {
   products: Product[] = [];
   dataLoaded=false;
+  filterText="";
   
 
   constructor(private productService :ProductService,
-    private activatedRoute:ActivatedRoute) {}
+    private activatedRoute:ActivatedRoute,
+    private toastrService:ToastrService,
+    private cartService:CartService) {}
 
   ngOnInit(): void {
 
@@ -41,6 +46,7 @@ export class ProductComponent implements OnInit {
         this.products=response.data
 
         this.dataLoaded=true;
+       
     });
     
    
@@ -56,5 +62,14 @@ export class ProductComponent implements OnInit {
     
    
   }
+  addToCart(product :Product)
+  {
+    this.toastrService.success("Sepete eklendi",product.productName)
+
+    this.cartService.addToCart(product);
+  }
+
+
+
 }
 
